167207b5f6f0cb06da14ab343b8c034e4c45b6d3
Sulaf Al Jabal <sulafaj@bu.edu>
2023-11-28 23:53:13 -0500

    src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate tsv;
 use std::collections::HashMap;
 use std::fs::File;
+use std::io::BufReader;
 
 
 fn main() {
@@ -9,6 +10,7 @@ fn main() {
 
     let mut x = "soc-redditHyperlinks-body.tsv";
     let file = File::open(x).expect("Could not open file");
+    let buf_reader = std::io::BufReader::new(file).lines();
     println!("{:?}", file);
 
 }
