b07d1b342b288c0658f56d75d7c8bb8fca9b88e7
Sulaf Al Jabal <140396223+sulafaljabal@users.noreply.github.com>
2023-11-28 23:40:23 -0500

    Testing whether commiting works on VSCode (idk how to do this)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate tsv;
+use std::collections::HashMap;
+use std::fs::File;
+
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!?");
+    //let mut wins = HashMap::<String,u16>::new();
+
     let mut x = "soc-redditHyperlinks-body.tsv";
+    let file = File::open(x).expect("Could not open file");
+
 }
 
